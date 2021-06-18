9a180a3bedf4b8321dd54d9b3c6415178380d345
leopansa <l_paniagua@hotmail.com>
2021-06-17 21:02:04 -0600

    Add project files.

---
+++ DrinkMachine.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+
+namespace DrinkMachine.Web.App_Start
+{
+    public class BundleConfig
+    {
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include( "~/Scripts/jquery-{version}.js"));
+        }
+    }
+}
